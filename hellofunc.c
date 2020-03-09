b07ec5de70467db567189e42bb01a8a6e5185f3f
theja0473 <theja0473@gmail.com>
2020-03-09 23:17:59 +0530

    Sample C code files

---
+++ hellofunc.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <hellomake.h>
+
+void myPrintHelloMake(void) {
+
+  printf("Hello makefiles!...\n");
+  printf("Hello Testing-1!...\n");
+  printf(""My Name is Theja!...\n");
+  printf("This is GSEP Team...\n");
+  printf("\n");
+  return;
+}
---
+++ hellomake.c
@@ -0,0 +1,14 @@
+#include <hellomake.h>
+
+int main() {
+
+  int a=5,b=6;
+  a=a+b;
+  b=b-a;
+  printf("Start Programming ...\n");
+  printf("a=%d,b=%d",a,b);
+  // call a function in another file
+  myPrintHelloMake();
+	printf("End Of Program...\n");
+  return(0);
+}
